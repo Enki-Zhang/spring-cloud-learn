96dc55378ac53881b20a6171bbc8b4be358953a2
Enki <17855378518@163.com>
2024-08-15 17:19:06 +0800

    nacos注册发现

---
+++ eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package cn.itcast.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @ClassName EurekaApplication
+ * @Description TODO
+ * @Author Enki
+ * @Date 2024/8/15 10:54
+ * @Verison 1.0
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
