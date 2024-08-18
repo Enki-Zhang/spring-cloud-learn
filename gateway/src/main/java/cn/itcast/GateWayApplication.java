eb0f8af8f01bf536c0a6225df0986e2146e2f445
Enki-Zhang <17855378518@163.com>
2024-08-18 21:29:04 +0800

    网关

---
+++ gateway/src/main/java/cn/itcast/GateWayApplication.java
@@ -0,0 +1,18 @@
+package cn.itcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Enki
+ * @ClassName GateWayApplication
+ * @Description: TODO
+ * @Date 2024/8/18 20:41
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
